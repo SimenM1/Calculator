b68c57c34163e41b8cd5e3db6de7ed2681770cc4
SimenM1 <156329770+SimenM1@users.noreply.github.com>
2024-01-31 22:29:52 +0000

    operate when pressing on operator added

--- javascript.js
+++ javascript.js
@@ -1,73 +1,127 @@
 //math
-let ans = 0
-let a = 3
-let b = 1
-let operator = "+"
+let ans = 0;
+let a = 3;
+let b = 1;
+let operator = "+";
 
 
 function add() {
-    ans = a + b
-    return ans
+    ans = a + b;
+    return ans;
 }
 
  function subtract() {
-    ans = a - b
-    return ans
+    ans = a - b;
+    return ans;
 }
 
 function multiply() {
-    ans = a*b
-    return ans
+    ans = a*b;
+    return ans;
 }
 
 function divide() {
-    ans = a/b
-    return ans
+    ans = a/b;
+    return ans;
 }
 
 function operate(operator, firstNr, secondNr) {
-    a = firstNr
-    b = secondNr
+    a = firstNr;
+    b = secondNr;
     switch(operator) {
         case "+":
-            return add()
+            return add();
 
         case "-":
-            return subtract()
+            return subtract();
 
         case "*":
-            return multiply()
+            return multiply();
 
         case "/":
-            return divide()
+            return divide();
    }
 }
 
 
-operate("+", 2, 6)
-console.log(ans)
+operate("+", 2, 6);
+console.log(ans);
 
 //gui
 
 function input(txt) {
     let inputDiv = document.querySelector(".display");
-    inputDiv.textContent = txt
+    inputDiv.textContent = txt;
 }
 
-postDisplay = 0
+postDisplay = 0;
 
 // input
-let nrBtns = document.querySelectorAll(".nrs")
-console.table(nrBtns)
+firstNr = 0
+let nrBtns = document.querySelectorAll(".nrs");
+console.table(nrBtns);
 for(i = 0; i < nrBtns.length; i++) {
-    let nr = nrBtns[i].textContent
+    let nr = nrBtns[i].textContent;
     nrBtns[i].addEventListener("click", () => {
-        preDisplay = document.querySelector(".display").textContent;
-        input(preDisplay + "" + nr);
-        postDisplay = document.querySelector(".display").textContent;
-        console.log(postDisplay)
-        return postDisplay
+        if (firstNr === ans) {
+            preDisplay = document.querySelector(".display").textContent;
+            input(nr);
+            postDisplay = document.querySelector(".display").textContent;
+            console.log(postDisplay);
+            ans = ""
+            return postDisplay;
+        }
+        else {
+            preDisplay = document.querySelector(".display").textContent;
+            input(preDisplay + "" + nr);
+            postDisplay = document.querySelector(".display").textContent;
+            console.log(postDisplay);
+            return postDisplay;
+        }
     })
 }
 
+clearBtn = document.querySelector("#clear")
+clearBtn.addEventListener('click', (event) => {
+    document.querySelector(".display").textContent = "";
+    postDisplay = 0
+    preDisplay = 0
+    firstNr = 0
+    secondNr = 0
+})
 
+let operatorBtn = document.querySelector("#left")
+operatorBtn.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (isButton) {
+        if (typeof firstNr !== "undefined" && firstNr !== 0) {
+            secondNr = +document.querySelector(".display").textContent;
+            operateFn()
+            document.querySelector(".display").textContent = ans;
+            operator = event.target.textContent;
+            firstNr = +ans
+            return firstNr, ans, operator
+        }
+        else {
+            firstNr = +postDisplay;
+            operator = event.target.textContent;
+            document.querySelector(".display").textContent = "";
+            return firstNr, operator;
+        }
+    }
+})
+
+
+operateBtn = document.querySelector("#operate")
+console.log(operateBtn)
+operateBtn.addEventListener("click", () => operateFn())
+
+function operateFn() {
+secondNr = +postDisplay
+ans = operate(operator, firstNr, secondNr)
+ansDisp = Math.floor((ans + 0.05)*10)/10
+document.querySelector(".display").textContent = ansDisp
+postDisplay = ans
+firstNr = ans
+return ans, firstNr, secondNr
+}
